e3c910854006d772ee9c3642391c28af7ca442f6
mejikuhibiniu1 <netflix.barengan19@gmail.com>
2025-08-12 11:12:16 +0700

    Upload lua file: 2462520.lua

---
+++ 2462520.lua
@@ -0,0 +1,17 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2462520 Manifest
+-- Name: I'm on Observation Duty 7
+-- Generated: 2025-06-06 01:52:09
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2462520) -- I'm on Observation Duty 7
+
+-- MAIN APP DEPOTS
+addappid(2462521, 1, "f4b0a0d97098ab3c5ce7a7aa4a7706a3a2068a75ac0797988bf046afb51fa8ac") -- Main Game Content (Windows Content)
+setManifestid(2462521, "7578748987376645139", 0)
+addappid(2462522, 1, "e1c314b4a036bbae337e561fc2aa3fa5fb3ae20522c67bb9dbcf49d10b603b2e") -- Game Content (Linux Binaries)
+setManifestid(2462522, "606944664167017574", 0)
+addappid(2462523, 1, "bcc77e77d2a3f8539c2b12090e9cd4941a0aa0f8dc1325afaac2c9e6e7a1b807") -- Game Content (Mac Content)
+setManifestid(2462523, "561406416410513796", 0)
